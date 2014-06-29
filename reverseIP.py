421725e5a328b8af43acb64700cf922d4fef4cc8
MaYaSeVeN <MaYaSeVeN@users.noreply.github.com>
2014-06-29 20:49:59 +0700

    Update reverseIP version 0.3
    refactoring to OOP style and add cloudflare detecting and so on

--- reverseIP.py
+++ reverseIP.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'MaYaSeVeN'
-__version__ = 'reverseIP version 0.2 ( http://mayaseven.com )'
+__author__ = 'Nop Phoomthaisong (aka @MaYaSeVeN)'
+__version__ = 'reverseIP version 0.3 ( http://mayaseven.com )'
 
 import urllib
 import urllib2
@@ -25,6 +25,7 @@ def main():
     parser.add_option("-d", "--disable", action="store_false",
                       help="set this option to disable to recheck is that the domain is in IP address",
                       default=True)
+
     (options, args) = parser.parse_args()
 
     if not options.key or (not options.file and len(args) == 0):
@@ -38,118 +39,165 @@ you need to..
 
     key = options.key
     recheck = options.disable
-    if not options.file:
-        for ip in args:
-            reverse_ip(ip, key, recheck)
-        exit(0)
+    file = options.file
+    reverseIP = RevereIP(args, key, recheck, file)
+    reverseIP.run()
 
-    filename = options.file
-    file_check(filename, key, recheck)
-
-
-def file_check(filename, key, recheck):
-    try:
-        file = open(filename, "r").read()
-        find_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', file)
-        ipl = list(set(find_ip))
-        for ip in ipl:
-            reverse_ip(ip, key, recheck)
-    except IOError:
-        print "[-] Error: File does not appear to exist."
-        exit(1)
+def stdout(log):
+    print log
 
 
-def reverse_ip(ip, key, recheck):
-    raw_domains_temp = []
-    domains = []
-    name = ""
-    if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip):
+class RevereIP:
+    def __init__(self, args, key, recheck, file):
+        self.domains = []
+        self.domain_numbers = 0
+        self.final_result = {}
+        self.ips_or_domains = set(args)
+        self.api_key = key
+        self.count = 0
+        self.log = stdout
+        self.recheck = recheck
+        self.file = file
+
+    def run(self):
+
+        if self.file:
+            self.ips_or_domains = self.file_opener()
+        while self.ips_or_domains:
+            ip_or_domain = self.ips_or_domains.pop()
+            self.reverse_ip(ip_or_domain)
+            self.log("[*] You got " + str(self.domain_numbers) + " domains to hack.")
+            self.domains = []
+
+    def file_opener(self):
         try:
-            name = ip
-            ip = socket.gethostbyname(ip)
-        except socket.gaierror:
-            print "[-] unable to get address for " + ip
+            file = open(self.file, "r").read()
+            find_ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', file)
+            ipl = set(find_ip)
+            return ipl
+        except IOError:
+            self.log("[-] Error: File does not appear to exist.")
+            exit(1)
+
+    def reverse_ip(self, ip_or_domain):
+        raw_domains_temp = []
+        name, ip = self.convert_domain_to_ip(ip_or_domain)
+        if not ip:
+            return
+        if self.check_ip_in_cloudflare(ip):
+            if name is not "":
+                self.log("\n[-] " + name + " hiding behind Cloudflare.")
+                return
+            self.log("[-] It's Cloudflare IP Address.")
             return
 
-    query = "IP:" + ip
-    print "\n[*] Host: " + ip + " " + name
-    count = 0
-    while 1:
-        raw_domains = bing_call_api(query, key, count)
-        if raw_domains == raw_domains_temp:
-            break
-        raw_domains_temp = raw_domains
-        if raw_domains == -1:
-            break
-        count += 100
-        if recheck:
-            check_domain_name_in_scope(raw_domains, ip)
-        else:
-            for l in raw_domains:
-                if l not in domains:
-                    print "[+] " + l.encode('utf8')
-                    domains.append(l)
+        query = "IP:" + ip
+        self.log("\n[*] Host: " + ip + " " + name)
 
+        self.count = 0
+        while 1:
+            raw_domains = self.bing_call_api(query)
+            if raw_domains == raw_domains_temp:
+                break
+            raw_domains_temp = raw_domains
+            if raw_domains == -1:
+                break
+            self.count += 100
+            if self.recheck:
+                self.check_domain_name_in_ip(raw_domains, ip)
+            else:
+                for l in raw_domains:
+                    if l not in self.domains:
+                        self.log("[+] " + l.encode('utf8'))
+                        self.domains.append(l)
+                self.final_result.update({ip: self.domains})
+                self.domain_numbers = len(self.domains)
 
-def bing_call_api(query, key, count):
-    domains = []
-    query = urllib.quote(query)
-    user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
-    credentials = (':%s' % key).encode('base64')[:-1]
-    auth = 'Basic %s' % credentials
-    url = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web?Query=%27' + query + '%27&$format=json' + '&$skip=' + str(
-        count)
-    request = urllib2.Request(url)
-    request.add_header('Authorization', auth)
-    request.add_header('User-Agent', user_agent)
-    request_opener = urllib2.build_opener()
-    try:
-        response = request_opener.open(request)
-    except urllib2.HTTPError, e:
-        if e.code == 401:
-            print '[-] Wrong API Key or not sign up to use Bing Search API'
-            print """
-you need to..
-1.register or use microsoft account for get Bing API key -> https://datamarket.azure.com/
-2.choose free plan 5,000 Transactions/month -> https://datamarket.azure.com/dataset/bing/search
-        """
+    def convert_domain_to_ip(self, ip_or_domain):
+        name = ""
+        if not re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip_or_domain):
+            try:
+                name = ip_or_domain
+                ip = socket.gethostbyname(ip_or_domain)
+                return name, ip
+            except socket.gaierror:
+                self.log("\n[-] unable to get address for " + ip_or_domain)
+                self.domain_numbers = 0
+                ip = None
+                return name, ip
+        return name, ip_or_domain
+
+    def bing_call_api(self, query):
+        domains = []
+        query = urllib.quote(query)
+        user_agent = 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; Trident/4.0; FDM; .NET CLR 2.0.50727; InfoPath.2; .NET CLR 1.1.4322)'
+        credentials = (':%s' % self.api_key).encode('base64')[:-1]
+        auth = 'Basic %s' % credentials
+        url = 'https://api.datamarket.azure.com/Bing/SearchWeb/v1/Web?Query=%27' + query + '%27&$format=json' + '&$skip=' + str(
+            self.count)
+        request = urllib2.Request(url)
+        request.add_header('Authorization', auth)
+        request.add_header('User-Agent', user_agent)
+        request_opener = urllib2.build_opener()
+        try:
+            response = request_opener.open(request)
+        except urllib2.HTTPError, e:
+            if e.code == 401:
+                self.log('[-] Wrong API Key or not sign up to use Bing Search API')
+                self.log("""
+    you need to..
+    1.register or use microsoft account for get Bing API key -> https://datamarket.azure.com/
+    2.choose free plan 5,000 Transactions/month -> https://datamarket.azure.com/dataset/bing/search
+            """)
+                exit(1)
+            self.log("[-] Something's gone wrong!!")
             exit(1)
-        print "[-] Something's gone wrong!!"
-        exit(1)
 
-    response_data = response.read()
-    json_result = json.loads(response_data)
-    result_list = json_result
-    if len(result_list['d']['results']) == 0:
-        count = -1
-        return count
+        response_data = response.read()
+        json_result = json.loads(response_data)
+        result_list = json_result
+        if len(result_list['d']['results']) == 0:
+            return -1
 
-    for i in range(len(result_list['d']['results'])):
-        domain = result_list['d']['results'][i]['DisplayUrl']
-        domain = domain.replace("http://", "").replace("https://", "")
-        rest = domain.split('/', 1)[0]
-        rest = rest.split(':', 1)[0]
-        domains.append(rest)
+        for i in range(len(result_list['d']['results'])):
+            domain = result_list['d']['results'][i]['DisplayUrl']
+            domain = domain.replace("http://", "").replace("https://", "")
+            rest = domain.split('/', 1)[0]
+            rest = rest.split(':', 1)[0]
+            domains.append(rest)
 
-    raw_domains = list(set(domains))
-    return raw_domains
+        raw_domains = list(set(domains))
+        return raw_domains
 
+    def check_domain_name_in_ip(self, raw_domains, ip):
+        for l in raw_domains:
+            if l not in self.domains:
+                self.domains.append(l)
+            else:
+                continue
+            if not (re.match('^[a-zA-Z0-9:.-]*$', l)):
+                self.log("[!] Can not recheck domain " + l.encode('utf8') + " please recheck it by hand.")
+                continue
+            ipc = socket.gethostbyname(l)
+            if ipc == ip:
+                self.log("[+] " + l.encode('utf8'))
+            else:
+                self.log("[!] " + l.encode('utf8') + " is on the other IP address, please recheck it by hand.")
+        self.final_result.update({ip: self.domains})
+        self.domain_numbers = len(self.domains)
 
-def check_domain_name_in_scope(raw_domains, ip):
-    domains = []
-    for l in raw_domains:
-        if l not in domains:
-            domains.append(l)
-        else:
-            continue
-        if not (re.match('^[a-zA-Z0-9:.-]*$', l)):
-            print "[!] Can not recheck domain " + l.encode('utf8') + " please check it by hand."
-            continue
-        ipc = socket.gethostbyname(l)
-        if ipc == ip:
-            print "[+] " + l.encode('utf8')
-        else:
-            print "[!] " + l.encode('utf8') + " is on the other IP address, please recheck it by hand."
+    def check_ip_in_cloudflare(self, ip):
+        cloudflare_ips = ['199.27.128.0/21', '173.245.48.0/20', '103.21.244.0/22', '103.22.200.0/22', '103.31.4.0/22',
+                          '141.101.64.0/18', '108.162.192.0/18', '190.93.240.0/20', '188.114.96.0/20',
+                          '197.234.240.0/22', '198.41.128.0/17', '162.158.0.0/15', '104.16.0.0/12']
+        ipaddr = int(''.join(['%02x' % int(x) for x in ip.split('.')]), 16)
+        for net in cloudflare_ips:
+            netstr, bits = net.split('/')
+            netaddr = int(''.join(['%02x' % int(x) for x in netstr.split('.')]), 16)
+            mask = (0xffffffff << (32 - int(bits))) & 0xffffffff
+            if (ipaddr & mask) == (netaddr & mask):
+                return True
+        return False
 
 
 if __name__ == "__main__":
